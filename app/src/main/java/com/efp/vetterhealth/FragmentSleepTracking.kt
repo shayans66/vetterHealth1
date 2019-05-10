22150b9ba2037022d0af3225127002767189c98f
Kyle <kylepetkovic@gmail.com>
2019-05-09 17:43:13 -0700

    Added bottom navigation, layout files, and fragment files for each section.

---
+++ app/src/main/java/com/efp/vetterhealth/FragmentSleepTracking.kt
@@ -0,0 +1,20 @@
+package com.efp.vetterhealth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class FragmentSleepTracking : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_sleep_tracking, container, false)
+
+
+        return view
+    }
+}
--- app/src/main/java/com/efp/vetterhealth/MainActivity.kt
+++ app/src/main/java/com/efp/vetterhealth/MainActivity.kt
@@ -4,24 +4,58 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+
+    var fragHelp = FragmentHelp()
+    var fragHome = FragmentHome()
+    var fragPost = FragmentPost()
+    var fragReminders = FragmentReminders()
+    var fragSleepTracking = FragmentSleepTracking()
+
+
+
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
+
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+
+                replaceFragment(fragHome, R.id.main_fragment)
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+            R.id.navigation_post -> {
+
+                replaceFragment(fragPost, R.id.main_fragment)
+
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_sleep_tracking -> {
+
+                replaceFragment(fragSleepTracking, R.id.main_fragment)
+
                 return@OnNavigationItemSelectedListener true
             }
+            R.id.navigation_reminders -> {
+
+                replaceFragment(fragReminders, R.id.main_fragment)
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_help -> {
+
+                replaceFragment(fragHelp, R.id.main_fragment)
+
+                return@OnNavigationItemSelectedListener true
+            }
+
         }
         false
     }
@@ -31,7 +65,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        replaceFragment(fragHome, R.id.main_fragment)
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
+
+    fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int){
+        supportFragmentManager.inTransaction { add(frameId, fragment) }
+    }
+
+
+    fun AppCompatActivity.replaceFragment(fragment: Fragment, frameId: Int) {
+        supportFragmentManager.inTransaction{replace(frameId, fragment)}
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+        beginTransaction().func().commit()
+    }
+
+
 }
